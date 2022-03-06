567669c187a9c2a4c4ee8354958dfb3deec0f62a
Alyxia Sother <alyxia@riseup.net>
2022-03-06 20:38:43 +0000

    [main] Implement markov command

--- Gemfile
+++ Gemfile
@@ -7,3 +7,5 @@ gem 'discordrb'
 gem "pg", "~> 1.3"
 
 gem "thread", "~> 0.2.2"
+
+gem "marky_markov", "~> 0.3.5"
--- main.rb
+++ main.rb
@@ -1,3 +1,6 @@
+require 'rubygems'
+require 'bundler/setup'
+
 require 'discordrb'
 
 require_relative 'database'
@@ -6,18 +9,31 @@ require_relative 'config'
 bot = Discordrb::Bot.new token: BOT_CONFIG[:token]
 botdb = Scrap::Database.new({ dbname: 'realbot' })
 
+MSGLOG_GET_ALL = %(select * from discord_log).freeze
+bot.message(with_text: "c!markov") do |e|
+  require 'marky_markov'
+  markov = MarkyMarkov::TemporaryDictionary.new
+  query = botdb.query(MSGLOG_GET_ALL) 
+  query.each do |row|
+    markov.parse_string row['mdata']
+  end
+  e.respond(markov.generate_1_sentences)
+end
+
 MSGLOG_NEW = %(insert into discord_log (cid, mid, uid, mtime, mdata) values ($1, $2, $3, $4, $5)).freeze
 MSGLOG_EDIT = %(update discord_log set etime = $3, edata = $4 where cid = $1 and mid = $2).freeze
 MSGLOG_DELETE = %(update discord_log set del = true where cid = $1 and mid = $2).freeze
 
 bot.message do |e|
-  botdb.query(MSGLOG_NEW, [
-    e.channel.id,
-    e.message.id,
-    e.author.id,
-    e.message.timestamp,
-    e.message.content
-  ])
+  if e.message.content != "c!markov"
+    botdb.query(MSGLOG_NEW, [
+      e.channel.id,
+      e.message.id,
+      e.author.id,
+      e.message.timestamp,
+      e.message.content
+    ])
+  end
 end
 
 bot.message_edit do |e|
